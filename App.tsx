6feeb098e9c1c327ac0ff6556d3d29f6174e5a41
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-10-03 13:09:12 +0200

    - Debut de la nav bar

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { Provider } from 'react-redux';
 import {store} from "./src/app/store";
 import Main from "./src/Main";
+import {NavigationContainer} from "@react-navigation/native";
 
 export default function App() {
   return (
       <Provider store={store}>
-          <Main/>
+          <NavigationContainer>
+            <Main/>
+          </NavigationContainer>
       </Provider>
   );
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -2,6 +2,7 @@ import UserInfo from "./components/UserInfo";
 import {StatusBar} from "expo-status-bar";
 import NameInput from "./components/NameInput";
 import {StyleSheet, View} from "react-native";
+import NavigationTab from "./components/NavigationTab";
 
 
 export default function Main() {
@@ -10,15 +11,14 @@ export default function Main() {
         <UserInfo/>
         <StatusBar style="auto" />
         <NameInput/>
+        <NavigationTab/>
     </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
     },
 });
