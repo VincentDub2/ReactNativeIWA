eac97f1e42291b551f0d43f85989cb0969a1e836
Kylian Thezenas <kylian.thezenas@etu.umontpellier.fr>
2024-10-17 09:06:48 +0200

    user page created

--- src/screens/UsersScreen.tsx
+++ src/screens/UsersScreen.tsx
@@ -1,27 +1,36 @@
 import React from "react";
-import {Text, View, Button, StyleSheet } from "react-native";
-import {useDispatch} from "react-redux";
-import {logout} from "../features/users/usersSlice";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import UserInfo from "../components/UserInfo";
+import { logout } from "../features/users/usersSlice";
+import { RootState } from "../app/store";
 
-export default function UsersScreen(  ) {
-    const dispatch = useDispatch();
+interface UsersScreenProps {
+	name: string;
+	profilePicture: string;
+}
 
-    const onLogout = () => {
-        // Déconnexion de l'utilisateur
-        dispatch(logout());
-    }
-    return (
-        <View >
-            <Text>Users Screen</Text>
-            <Button title="Déconnexion" onPress={onLogout} />
-        </View>
-    );
+export default function UsersScreen() {
+	const dispatch = useDispatch();
+
+	const onLogout = () => {
+		// Déconnexion de l'utilisateur
+		dispatch(logout());
+	};
+	return (
+		<View style={styles.container}>
+			<UserInfo
+				name={useSelector((state: RootState) => state.users.name)}
+				profilePicture={require("../../assets/images/quack-with-tent-background.png")}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
 });
