0c2c78c317ddb1908c7f998a612e80a412e9dc2c
Vincent Dubuc <cerise.litchis.0z@icloud.com>
2024-11-24 14:25:10 +0100

    set up test

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "module:metro-react-native-babel-preset", // Toujours placer ce preset en premier pour React Native
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }], // Expo doit venir après Metro
       "nativewind/babel",
+    ],plugins: [
+      'module:react-native-dotenv',
     ],
   };
 };
